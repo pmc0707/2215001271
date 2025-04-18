69bbeb2b71de90dd11d94bd0d2757e47000fb69a
pmc0707 <pranshugoodboy@gmail.com>
2025-04-18 12:00:51 +0530

    final project push

--- src/AverageCalculator.js
+++ src/AverageCalculator.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE = 'http://20.244.56.144/evaluation-service';
+const API_BASE = 'http://20.244.56.144/evaluation-service/';
 
 const endpoints = {
   fibo: 'fibo',
